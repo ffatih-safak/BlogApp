43c93ec7f434185a90af28f950930a90c6e3077d
Fatih <ffatihsafak@gmail.com>
2021-09-07 18:00:20 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content //İçerik
+    {
+        [Key]
+        public int ContentID { get; set; }
+        [StringLength(1000)]
+        public string ContentValue { get; set; } //Yazı
+
+        public DateTime ContentDate { get; set; }
+
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
